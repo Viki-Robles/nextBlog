6d5a170d01c901a7467bbb57e64dd7fd12828d1d
Vicky Vasilopoulou <vickyvasilopoulou@Vickys-MacBook.local>
2020-09-29 16:06:18 +0100

    fixing error on build

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Layout, { siteTitle } from './components/layout';
 import utilStyles from './styles/utils.module.css';
 import Link from 'next/link';
 import { getSortedPostsData } from '../lib/posts';
-import Date from './components/date/date';
+import Date from './components/date';
 
 export default function Home({ allPostsData }) {
   return (
